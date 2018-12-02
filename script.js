506de9f56016edd3865be75b2f7479c57bdd0dfc
Ezra-Bernstein <33584779+Ezra-Bernstein@users.noreply.github.com>
2018-12-01 19:07:27 -0500

    yay

---
+++ script.js
@@ -0,0 +1,23 @@
+function url1(){
+   window.location.assign("google.com"")
+   console.log("yikes")
+  // }
+}
+
+function url2(){
+   window.location.assign("google.com")
+   console.log("yikes")
+  // }
+}
+
+function url3(){
+   window.location.assign("google.com")
+   console.log("yikes")
+  // }
+}
+
+function url4(){
+   window.location.assign("google.com")
+   console.log("yikes")
+  // }
+}
